1eab531bd6d015793fcfe01a829f0efa7e2aeab6
HARVEYNASH\locnguyenh <nguyenhuuloc304@gmail.com>
2021-10-06 23:23:25 +0700

    update code

---
+++ IDS/IDS/Startup.cs
@@ -0,0 +1,60 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDS
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            services.AddIdentityServer()
+                .AddInMemoryClients(SeedData.Clients)
+                .AddInMemoryIdentityResources(SeedData.IdentityResources)
+                .AddInMemoryApiResources(SeedData.ApiResources)
+                .AddInMemoryApiScopes(SeedData.ApiScopes)
+                .AddTestUsers(SeedData.Users)
+                .AddDeveloperSigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # identityserver4
+
+# Documents
+https://www.identityserver.com/articles/online-tutorials-for-getting-started-with-identityserver4-and-adminui
